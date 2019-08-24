5ba4baf1a67525b3603e0586bdf1e2f472553a48
zainahmed1 <50447349+zainahmed1@users.noreply.github.com>
2019-08-24 18:03:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ var x = setInterval(function () {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+     
+    document.querySelector("i").innerHTML = days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
 
     if (distance < 0) {

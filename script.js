454ffe97b1837a58b3ceef8b1ea2f3d69cd1b0c5
zainahmed1 <50447349+zainahmed1@users.noreply.github.com>
2019-09-07 13:43:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let sound = new Audio("sounds/instru.mp3");
 var countDownDate = new Date("NOV 16, 2019 04:00:00").getTime();
 
 var x = setInterval(function () {
-    // sound.play();
-    // sound.volume = 0.2;
+    sound.play();
+    sound.volume = 0.2;
 
-    var now = new Date().getTime(); 
+    var now = new Date().getTime();
 
     var distance = countDownDate - now;
 
@@ -14,14 +14,14 @@ var x = setInterval(function () {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-     
+
     document.querySelector("em").innerHTML = days + "d " + hours + "h "
         + minutes + "m " + seconds + "s ";
-        
+
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "EXPIRED";
-        
+
     }
 }, 1000);
- 
+
